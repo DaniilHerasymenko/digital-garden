b11ed8630a674673c8d1f2f036d226e95f20956c
DaniilHerasymenko <herasymenkodaniil@gmail.com>
2025-09-11 13:34:42 +0300

    Quartz sync: Sep 11, 2025, 1:34 PM

--- quartz/components/Footer.tsx
+++ quartz/components/Footer.tsx
@@ -1,6 +1,5 @@
 import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } from "./types"
 import style from "./styles/footer.scss"
-import { version } from "../../package.json"
 import { i18n } from "../i18n"
 
 interface Options {
@@ -14,7 +13,7 @@ export default ((opts?: Options) => {
     return (
       <footer class={`${displayClass ?? ""}`}>
         <p>
-          {i18n(cfg.locale).components.footer.createdWith}{" "}
+          {i18n(cfg.locale).components.footer}{" "}
           <a href="https://neboskhyl.xyz/">neboskhyl</a> © {year}
         </p>
         <ul>
